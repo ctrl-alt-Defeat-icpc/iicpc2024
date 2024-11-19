09285b079af4623bed419a665fff6ef8b9fa8d0e
EnAnsari <rahmat2022a@gmail.com>
2024-11-19 16:13:55 +0330

    Updated by update.py

---
+++ rahmat/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int N = 1e5 + 5;
+
+void Main() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    // int t; cin >> t; while(t--) Main();
+    Main();
+    return 0;
+}
