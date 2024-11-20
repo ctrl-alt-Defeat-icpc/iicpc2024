1def21e2cc8fd9274a521676721dd53c80cc12a1
EnAnsari <rahmat2022a@gmail.com>
2024-11-20 16:29:48 +0330

    Updated by update.py

---
+++ rahmat/d.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+const int N = 1e6 + 5;
+int n, q, cur, dcur, dcur_neg;
+string choice;
+ull server[N], ans, value, diag;
+
+void Main() {
+    cin >> n >> q;
+    while(q--) {
+        cin >> choice;
+        if(choice[0] == 'a') {
+            cin >> value;
+            server[cur++] = value;
+            ans += value;
+        }
+        else {
+            if(dcur < cur)
+                diag += server[dcur++];
+            if(dcur >= n && dcur_neg < cur)
+                diag -= server[dcur_neg++];
+            
+            ans += diag;
+        }
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        Main();
+    return 0;
+}
