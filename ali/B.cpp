a01a06a8ffee3d7a5062b07d65d41de1c0397a63
AliBinary <AliGhanbariCs@gmail.com>
2024-11-20 14:12:26 +0330

    ali

---
+++ ali/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int N = 1e5 + 5;
+
+void Main()
+{
+    int n;
+    cin >> n;
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == c || a == d || b == c || b == d)
+        cout << 3;
+    else
+        cout << 4;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        Main();
+    return 0;
+}
